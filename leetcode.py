bf640ebb6b2b26b67c75cf19df0dc07797c3347e
Sahil Mishra <sahilmishra780@gmail.com>
2023-11-28 22:27:16 -0800

    least interval

--- leetcode.py
+++ leetcode.py
@@ -1,64 +1,34 @@
+from collections import *
 from typing import *
-
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.word = ""
+import heapq
 
 class Solution:
-    def add(self, word):
-        node = self.root
-        for c in word:
-            if c not in node.children:
-                node.children[c] = TrieNode()
-            node = node.children[c]
-        node.word = word
-    
-    def search(self, word):
-        node = self.root
-        for c in word:
-            if c not in node.children:
-                return False
-            node = node.children[c]
-        return len(node.word) > 0
-    
-    def buildTrie(self, words):
-        self.root = TrieNode()
-        for word in words:
-            self.add(word)
+    def leastInterval(self, tasks: List[str], idleTime: int) -> int:
+        count = Counter(tasks)
+        maxHeap = [(-cnt, char) for char, cnt in count.items()]
+        heapq.heapify(maxHeap)
+        q = deque()
+        time = 0
+        str = ""
+        while q or maxHeap:
+            time += 1
+            currTask = "idle"
+            if maxHeap:
+                # use 1 count from freq
+                freq, currTask = heapq.heappop(maxHeap)
+                freq += 1
+                if freq: 
+                    # i.e. there are more tasks of this type to process
+                    # now append freq and time when this task
+                    # will be available to use
+                    q.append([freq, time + idleTime, currTask])
+            if q and q[0][1] == time:
+                # no char is used
+                # char is still "idle"
+                f, _, t = q.popleft()
+                heapq.heappush(maxHeap, (f, t))
+            str += currTask + "->"
+        # print(str)    
+        return time
 
-    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        self.buildTrie(words)
-
-        R, C = len(board), len(board[0])
-        ans = []
-        def bt(r, c, node):
-            if node.word:
-                ans.append(node.word)
-                node.word = ""
-            if (
-                r < 0 or
-                r >= R or
-                c < 0 or
-                c >= C
-            ):
-                return
-            s = board[r][c]
-            if s not in node.children:
-                return
-            board[r][c] = "#"
-            node = node.children[s]
-            bt(r - 1, c, node)
-            bt(r + 1, c, node)
-            bt(r, c - 1, node)
-            bt(r, c + 1, node)
-            board[r][c] = s
-
-        for r in range(R):
-            for c in range(C):
-                s = board[r][c]
-                if s in self.root.children:
-                    bt(r, c, self.root)
-        return ans
-
-Solution().findWords([["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], ["oath","pea","eat","rain"])
+Solution().leastInterval(["A","A","A","B","B","B"], 3)
