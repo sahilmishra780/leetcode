e118b8b0f852cef17b93ac85b11de0c04f9c5ace
Sahil Mishra <sahilmishra780@gmail.com>
2023-12-13 21:31:19 -0800

    longest palindrome substring recursion and updated dp

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -21,39 +21,73 @@ class Solution:
     
     def longestPalindromeDP(self, s: str) -> str:
         n = len(s)
-
-        # dp[i][j] = True indicates that the string formed by s[i:j+1] (i.e. from s[i] to s[j] (inclusive))
-        # is a palindrome
         dp = [[False] * n for _ in range(n)]
+        
+        maxLen = 1
+        maxLenStartIdx = 0
 
-        # single characters are palindromes
-        for i in range(n):
-            dp[i][i] = True
+        for left in range(n - 1, -1, -1):
+            for right in range(left, n):
 
-        longest_palindrome_start, longest_palindrome_len = 0, 1
-
-        for right in range(0, n): # right can be 0, 1, 2, ..., n - 1
-            for left in range(right - 1, -1, -1): # left can be right - 1, right - 2, ..., 0
-
+                # Boundary characters match
                 if s[left] == s[right]:
-                    # chars pointed by left and right match. 
-                    
-                    # Is the string formed from s[left+1] .. s[right - 1]?
-                    # Yes if
-                    # 1. right = left + 1 OR (no string formed therefore a palindrom)
-                    # 2. dp[left + 1][right - 1] is True (we've previously determined the enclosed string to be a palindrom)
-                    if right == left + 1 or dp[left + 1][right - 1]:
+                    # Now, there are multiple cases for the substring formed via s[left,right]
+                    # 1. substring is 1 char long i.e. left === right In this case yes it is a palindrome
+                    # 2. substring is 2 char long i.e. left + 1 == right. In this case yes it is a palindrome as s[left] == s[right]
+                    # 3. substring is 3 char long i.e  left + 2 == right. In this case again it is a palindrome as s[left] == s[right]
+                    # 4. substring is > 3 char long. In this case the answer depends on the substring formed via s[left + 1, right - 1]
+
+                    # Case 1, 2, and 3 can be summarized as right - left <= 2 or right - 1 <= left + 1
+                    # Case 4 can be determined from the dp
+                    if (right - 1 <= left + 1 or dp[left + 1][right - 1]):
                         dp[left][right] = True
-                        current_palindrome_len = right - left + 1
-                        if current_palindrome_len > longest_palindrome_len:
-                            longest_palindrome_start = left
-                            longest_palindrome_len = current_palindrome_len
-        return s[longest_palindrome_start:longest_palindrome_start + longest_palindrome_len]
+                        curLen = right - left + 1
+                        if curLen > maxLen:
+                            maxLen = curLen
+                            maxLenStartIdx = left
+                            
+        return s[maxLenStartIdx: maxLenStartIdx + maxLen]   
+    
+    def longestPalindromeRecursion(self, s: str) -> str:
+        n = len(s)
+        memo = [[-1] * n for _ in range(n)]
+        def isPali(s, l, r):
+            if l >= r:
+                return True
+            # now l < r, i.e substring is atleast 2 chars
+            if s[l] != s[r]:
+                return False
+            # s[l] and s[r] are identical and substring is atleast 2 chars
+            
+            # Check if we have a cached result
+            if memo[l][r] != -1:
+                return memo[l][r]
+            
+            # No cached result. Update cache
+            memo[l][r] = isPali(s, l + 1, r - 1)
+            
+            return memo[l][r]
+        
+        maxLen = 1
+        maxLenStartIdx = 0
+
+        for left in range(n):
+            for right in range(left, n):
+                if s[left] == s[right] and isPali(s, left + 1, right - 1):
+                    curLen = right - left + 1
+                    if curLen > maxLen:
+                        maxLen = curLen
+                        maxLenStartIdx = left
+
+        return s[maxLenStartIdx: maxLenStartIdx + maxLen]   
 
 print(Solution().longestPalindromeSlidingWindow("babad"))
 print(Solution().longestPalindromeSlidingWindow("cbbd"))
 
 print(Solution().longestPalindromeDP("babad"))
 print(Solution().longestPalindromeDP("cbbd"))
+
+print(Solution().longestPalindromeRecursion("babad"))
+print(Solution().longestPalindromeRecursion("cbbd"))
 
 
