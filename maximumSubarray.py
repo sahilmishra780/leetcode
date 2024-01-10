ff7e391a78051f6e01a7353cf2c2bffd417d0a1b
Sahil Mishra <sahilmishra780@gmail.com>
2024-01-10 15:11:58 +0530

    maximum subarray

---
+++ maximumSubarray.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        currSum = 0
+        maxSum = float('-inf')
+        for val in nums:
+            currSum += val
+            maxSum = max(maxSum, currSum)
+            currSum = max(0, currSum)
+        return maxSum
+soln = Solution()
+assert soln.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) == 6
+assert soln.maxSubArray([1]) == 1
+assert soln.maxSubArray([5,4,-1,7,8]) == 23
